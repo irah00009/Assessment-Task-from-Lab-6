00502270253773cca5a2bb851b871c478bd228b8
irah00009 <167538101+irah00009@users.noreply.github.com>
2024-10-07 20:12:41 -0700

    Add files via upload

---
+++ xmlData.js
@@ -0,0 +1,28 @@
+// xmlData.js
+const productsXML = `
+<products>
+    <product>
+        <id>1</id>
+        <name>Product 1</name>
+        <description>This is the description for Product 1.</description>
+        <price>29.99</price>
+        <imageUrl>https://bermorzone.com.ph/wp-content/uploads/2023/06/geforce-rtx-4060-gaming-x-8g-649a4abc5fad6.webp</imageUrl>
+    </product>
+    <product>
+        <id>2</id>
+        <name>Product 2</name>
+        <description>This is the description for Product 2.</description>
+        <price>39.99</price>
+        <imageUrl>https://img.lazcdn.com/g/p/980de08913478a4687744374a4cd1b04.jpg_720x720q80.jpg</imageUrl>
+    </product>
+    <product>
+        <id>3</id>
+        <name>Product 3</name>
+        <description>This is the description for Product 3.</description>
+        <price>49.99</price>
+        <imageUrl>https://pcx.com.ph/cdn/shop/files/VC-MSI-RTX3050-VENTUS-2X-OCV1-8GB-GDDR6-128BIT-1.jpg?v=1709867488&width=600</imageUrl>
+    </product>
+</products>
+`;
+
+module.exports = productsXML; // Exporting the XML data
