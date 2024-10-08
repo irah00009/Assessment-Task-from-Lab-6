d06bcc0764c4b304a2ba4543bf1974e1f3f8e27c
irah00009 <167538101+irah00009@users.noreply.github.com>
2024-10-07 20:15:14 -0700

    Add files via upload

---
+++ jsonData.js
@@ -0,0 +1,26 @@
+// jsonData.js
+const products = [
+    {
+        id: 1,
+        name: 'Product 1',
+        description: 'This is the description for Product 1.',
+        price: 29.99,
+        imageUrl: 'https://bermorzone.com.ph/wp-content/uploads/2023/06/geforce-rtx-4060-gaming-x-8g-649a4abc5fad6.webp'
+    },
+    {
+        id: 2,
+        name: 'Product 2',
+        description: 'This is the description for Product 2.',
+        price: 39.99,
+        imageUrl: 'https://img.lazcdn.com/g/p/980de08913478a4687744374a4cd1b04.jpg_720x720q80.jpg'
+    },
+    {
+        id: 3,
+        name: 'Product 3',
+        description: 'This is the description for Product 3.',
+        price: 49.99,
+        imageUrl: 'https://pcx.com.ph/cdn/shop/files/VC-MSI-RTX3050-VENTUS-2X-OCV1-8GB-GDDR6-128BIT-1.jpg?v=1709867488&width=600'
+    }
+];
+
+module.exports = products; // Exporting the product data
